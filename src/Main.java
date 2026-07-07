61dd62fbf3a0901f44080562980ba3e042da17db
Gaurav Kumar <gk8499321@gmail.com>
2026-07-07 09:17:19 +0530

    todays problem

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        String s = "57761";
-        for(int i=0;i<s.length();i++){
-            int x=s.charAt(i)-'0';
-                        
+    public static long sumAndMultiply(int n) {
+        String s=n+"";
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<s.length();i++) {
+            if (s.charAt(i) != '0') {
+                ans.append(s.charAt(i));
+            }
         }
+        int ans1=Integer.parseInt(ans.toString());
+        int number=ans1;
+        int sum=0;
+        while(ans1>0){
+            sum+=ans1%10;
+            ans1/=10;
+        }
+        long realAns =(long)number*(long)sum;
+        return realAns;
     }
+
+    public static void main(String[] args) {
+        System.out.println(sumAndMultiply(65463628));
+    }
+
 }
