78b72e91a8ebec3edd249cfb8cb7f24020fcacf4
Gaurav Kumar <gk8499321@gmail.com>
2026-07-09 00:58:08 +0530

    two pointers approach

---
+++ src/twoPointer.java
@@ -0,0 +1,26 @@
+public class twoPointer {
+    public static int getCommon(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int i=0;
+        int j=0;
+        while(i<n&&j<m){
+            if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[]nums1={1,1,2};
+        int[]nums2={2,4};
+        System.out.println(getCommon(nums1,nums2));
+    }
+}
